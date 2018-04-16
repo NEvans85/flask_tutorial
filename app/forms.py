316ea3c5827e3d7e45c9cfc5ed4f97cdc395a15f
Nicholas Evans <nevans85.pro@gmail.com>
2018-04-16 09:42:34 -0700

    add password reset feature, not working yet

--- app/forms.py
+++ app/forms.py
@@ -47,10 +47,12 @@ class PostForm(FlaskForm):
     submit = SubmitField('Submit')
 
 class ResetPasswordRequestForm(FlaskForm):
-    email = StringField('Email',validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Request Password Reset')
+
 
 class ResetPasswordForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Request Password Reset')
--- app/routes.py
+++ app/routes.py
@@ -139,9 +139,10 @@ def reset_password_request():
         user = User.query.filter_by(email=form.email.data).first()
         if user:
             send_password_reset_email(user)
-        flash('Check your email for the instructions to reset your password.')
+        flash('Check your email for the instructions to reset your password')
         return redirect(url_for('login'))
-    return render_template('reset_password_request.html', title='Reset Password', form=form)
+    return render_template('reset_password_request.html',
+                           title='Reset Password', form=form)
 
 @app.route('/reset_password/<token>', methods=['GET', 'POST'])
 def reset_password(token):
@@ -151,9 +152,9 @@ def reset_password(token):
     if not user:
         return redirect(url_for('index'))
     form = ResetPasswordForm()
-    if form.validate_on_submit:
+    if form.validate_on_submit():
         user.set_password(form.password.data)
         db.session.commit()
         flash('Your password has been reset.')
         return redirect(url_for('login'))
-    return render_template('reser_password.html', form=form)
+    return render_template('reset_password.html', form=form)
